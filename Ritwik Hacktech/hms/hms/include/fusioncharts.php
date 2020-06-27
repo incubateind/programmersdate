e249c799ca2e45c63704f888ce6ea4496c5d58e6
Ritwik Sinha <rittiksinha12@gmail.com>
2020-06-27 11:20:05 +0530

    Add files via upload

---
+++ Ritwik Hacktech/hms/hms/include/fusioncharts.php
@@ -0,0 +1,8 @@
+// Include FusionCharts core file
+<script type="text/javascript" src="https://cdn.fusioncharts.com/fusioncharts/latest/fusioncharts.js"></script>
+
+// Include FusionCharts maps file
+<script type="text/javascript" src="https://cdn.fusioncharts.com/fusioncharts/latest/fusioncharts.maps.js"></script>
+
+// Include FusionCharts Theme file
+<script type="text/javascript" src="https://cdn.fusioncharts.com/fusioncharts/latest/themes/fusioncharts.theme.fusion.js"></script>
